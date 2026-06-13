319fc19679cc88abacedc9a11d02eebd7d66b514
sachin patil <sachinppatil02@gmail.com>
2026-06-13 20:00:16 +0530

    fetch remote to local

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,27 @@
-output "db_endpoint" {
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_1" {
+  value = aws_subnet.public_1.id
+}
+
+output "public_subnet_2" {
+  value = aws_subnet.public_2.id
+}
+
+output "private_subnet_1" {
+  value = aws_subnet.private_1.id
+}
+
+output "private_subnet_2" {
+  value = aws_subnet.private_2.id
+}
+
+output "rds_endpoint" {
   value = aws_db_instance.postgres.endpoint
 }
 
-output "db_identifier" {
+output "rds_identifier" {
   value = aws_db_instance.postgres.id
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,5 @@
-db_password = "YourStrongPasswd"
+aws_region = "us-east-1"
+
+db_username = "postgres"
+
+db_password = "MySecurePassword123!"
