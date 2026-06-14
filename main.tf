669604c849a8c9b41fa1d27005b3ccb05af8c36b
sachin patil <sachinppatil02@gmail.com>
2026-06-14 11:30:34 +0530

    new changes

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_db_instance" "postgres" {
   username               = var.db_username
   password               = var.db_password
   publicly_accessible    = false
-  skip_final_snapshot    = false
+  skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.default.name
 
